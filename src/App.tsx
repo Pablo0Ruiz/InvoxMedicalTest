58a56ab09cb29f01e99bc7625e5d8094008bbbe0
Pablo Ruiz <145439653+Pablo0Ruiz@users.noreply.github.com>
2026-02-11 03:19:24 +0100

    base proyect

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold underline">
+          Invox Medical Test
+        </h1>
+      </div>
+
+    </>
+  ) 
+}
+
+export default App
